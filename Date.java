90647f71299a25fe55dd6ff6ac8b0934619c2b90
Dipu Kumar <96506977+dipukr@users.noreply.github.com>
2023-01-03 11:22:07 +0530

    Update Date.java

--- Date.java
+++ Date.java
@@ -11,42 +11,36 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-public class ControllerLayer {
+public class Date {
 
-	@Setter
-	@Getter
-	@ToString
-	public class QuantumData {
-
-		private Date fromDate;
-		private Date toDate;
-		
-		private Time fromTime;
-		private Time toTime;
-		
-		private Float quantum;
+	public class Period {
+		public Date fromDate;
+		public Date toDate;
+		public Time fromTime;
+		public Time toTime;
+		public Float quantum;
 	}
 	
-	public boolean isOverlaping(List<QuantumData> quantumDatas) throws Exception {
+	public boolean isOverlaping(List<Period> periods) throws Exception {
 		Map<Integer, Map<Integer, Map<Integer, Float>>> map = new TreeMap<>();
 		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
 		Date currDate = formatter.parse("24-08-2022");
 
-		for (QuantumData quantumData: quantumDatas) {
-			int month = DateUtil.getMonth(quantumData.getFromDate());
+		for (Period period: periods) {
+			int month = DateUtil.getMonth(quantumData.fromDate);
 			if (!map.containsKey(month))
 				map.put(month, new TreeMap<Integer, Map<Integer, Float>>());
-			int startDay = DateUtil.getDay(quantumData.getFromDate());
-			int endDay = DateUtil.getDay(quantumData.getToDate());
+			int startDay = DateUtil.getDay(period.fromDate);
+			int endDay = DateUtil.getDay(period.toDate);
 			for (int day = startDay; day <= endDay; day++) {
 				if (!map.get(month).containsKey(day))
 					map.get(month).put(day, new TreeMap<Integer, Float>());
-				int startMinute = DateUtil.getMinute(quantumData.getFromTime());
-				int endMinute = DateUtil.getMinute(quantumData.getToTime());
+				int startMinute = DateUtil.getMinute(period.fromTime);
+				int endMinute = DateUtil.getMinute(period.toTime);
 				for (int minute = startMinute; minute <= endMinute; minute++) {
 					if (map.get(month).get(day).containsKey(minute)) {
 						return true;
-					else map.get(month).get(day).put(minute, quantumData.getQuantum()); 
+					else map.get(month).get(day).put(minute, period.quantum); 
 				}
 			}
 		}		
